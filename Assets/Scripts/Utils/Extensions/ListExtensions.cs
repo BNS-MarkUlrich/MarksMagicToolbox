5e3ff07faa84e33808c20dd26de2995c70b35e2e
Mark Ulrich <71002222+BNS-MarkUlrich@users.noreply.github.com>
2022-06-01 15:01:42 +0200

    Added ListExtensions with Contains method overload

---
+++ Assets/Scripts/Utils/Extensions/ListExtensions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ListExtensions
+{
+    /// <summary>
+    /// Checks if compareTarget includes target List.
+    /// </summary>
+    /// <param name="target"></param>
+    /// <param name="compareTarget"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static bool Contains<T>(this IList<T> target, IList<T> compareTarget)
+    {
+        foreach (var tag in compareTarget)
+        {
+            if (!target.Contains(tag)) return false;
+        }
+            
+        return true;
+    }
+}
