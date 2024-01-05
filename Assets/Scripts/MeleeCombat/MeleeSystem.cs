47f96afe96495b0a8acf837e5eb4e35e310d527e
Mark Ulrich <71002222+BNS-MarkUlrich@users.noreply.github.com>
2024-01-05 18:59:05 +0100

    Added Cardinal Directions Enum to ChooseDirection

--- Assets/Scripts/MeleeCombat/MeleeSystem.cs
+++ Assets/Scripts/MeleeCombat/MeleeSystem.cs
@@ -7,9 +7,13 @@ public class MeleeSystem : MonoBehaviour
 
     private Vector2 attackDirection;
     private Vector2 mouseDirection;
+    private CardinalDirections currentDirection = CardinalDirections.Up;
+
+    [SerializeField] private SerializableDictionary<CardinalDirections, Vector2> directionVectors;
 
     public Agent OwningAgent { get; set; }
     public Vector2 AttackDirection => attackDirection;
+    public CardinalDirections CurrentDirection => currentDirection;
 
     private void Start() 
     {
@@ -41,15 +45,44 @@ public class MeleeSystem : MonoBehaviour
         mouseDirection.Normalize();
 
         // snap attack directon to 4 cardinal directions
-        if (Mathf.Abs(mouseDirection.x) > Mathf.Abs(mouseDirection.y))
-            attackDirection.Set(mouseDirection.x, 0f);
-        else if (Mathf.Abs(mouseDirection.y) > Mathf.Abs(mouseDirection.x))
-            attackDirection.Set(0f, mouseDirection.y);
-
-        attackDirection.Normalize();
+        SnapDirection(mouseDirection);
 
         myWeapon.SetAttackDirection(attackDirection);        
         myWeapon.SetBlockDirection(attackDirection);
+    }
+
+    private void SnapDirection(Vector2 direction)
+    {
+        if (direction.x > 0.5f)
+        {
+            currentDirection = CardinalDirections.Right;
+            directionVectors.TryGetValue(currentDirection, out attackDirection);
+            print("Right");
+        }
+        else if (direction.x < -0.5f)
+        {
+            currentDirection = CardinalDirections.Left;
+            directionVectors.TryGetValue(currentDirection, out attackDirection);
+            print("Left");
+        }
+        else if (direction.y > 0.5f)
+        {
+            currentDirection = CardinalDirections.Up;
+            directionVectors.TryGetValue(currentDirection, out attackDirection);
+            print("Up");
+        }
+        else if (direction.y < -0.5f)
+        {
+            currentDirection = CardinalDirections.Down;
+            directionVectors.TryGetValue(currentDirection, out attackDirection);
+            print("Down");
+        }
+        else
+        {
+            directionVectors.TryGetValue(currentDirection, out attackDirection);
+        }
+
+        attackDirection.Normalize();
     }
 
     public void SetAttackDirection(Vector2 direction)
