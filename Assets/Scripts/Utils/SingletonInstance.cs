5363c0e082e2f6564aa81b68515e416f87ea2518
Mark Ulrich <71002222+BNS-MarkUlrich@users.noreply.github.com>
2025-07-17 22:55:39 +0200

    Updated SingletonInstance to use namespace

--- Assets/Scripts/Utils/SingletonInstance.cs
+++ Assets/Scripts/Utils/SingletonInstance.cs
@@ -1,42 +1,45 @@
 using UnityEngine;
 
-public abstract class SingletonInstance<T> : MonoBehaviour where T : Component
+namespace MarkUlrich.Utils
 {
-    private static T _instance;
-    public static T Instance
+    public abstract class SingletonInstance<T> : MonoBehaviour where T : Component
     {
-        get
-        {            
-            if (_instance != null)
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {            
+                if (_instance != null)
+                    return _instance;
+
+                _instance = FindObjectOfType<T>();
+
+                if (_instance != null)
+                    return _instance;
+                
+                GameObject container = new(typeof(T).Name);
+                _instance = container.AddComponent<T>();
+
                 return _instance;
-
-            _instance = FindObjectOfType<T>();
-
-            if (_instance != null)
-                return _instance;
-            
-            GameObject container = new(typeof(T).Name);
-            _instance = container.AddComponent<T>();
-
-            return _instance;
+            }
         }
-    }
 
-    protected virtual void OnEnable() => InitSingletonInstance();
+        protected virtual void OnEnable() => InitSingletonInstance();
 
-    /// <summary>
-    /// Initialises the Singleton Instance.
-    /// </summary>
-    /// <returns>The Singleton Instance.</returns>
-    protected virtual T InitSingletonInstance()
-    {
-        transform.parent = transform.root;
+        /// <summary>
+        /// Initialises the Singleton Instance.
+        /// </summary>
+        /// <returns>The Singleton Instance.</returns>
+        protected virtual T InitSingletonInstance()
+        {
+            transform.parent = transform.root;
 
-        if (Instance != this)
-            Destroy(gameObject);
-        else
-            DontDestroyOnLoad(gameObject);
+            if (Instance != this)
+                Destroy(gameObject);
+            else
+                DontDestroyOnLoad(gameObject);
 
-        return Instance;
+            return Instance;
+        }
     }
 }
